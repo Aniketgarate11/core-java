3cf7774467d8f5cba3511d75548eaa356862e55f
Aniket Garate <120038313+Aniketgarate11@users.noreply.github.com>
2022-12-07 21:17:27 +0530

    changes

---
+++ Firstproject/src/com/packages/Demo.java
@@ -0,0 +1,26 @@
+package com.packages;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		String a  = "Delhi" ;
+		switch (a) {
+		case "mumbai":
+			System.out.println("Financial city");
+			break;
+		case "Banglore":
+			System.out.println("Ciber city");
+			break;
+		case "Delhi":
+			System.out.println("capital city of India");
+			break;			
+
+		default:
+			System.out.println("May be other city in India");
+			break;
+		}
+
+	}
+
+}
